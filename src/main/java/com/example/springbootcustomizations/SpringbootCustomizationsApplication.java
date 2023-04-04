a7cb59980c4aa6b8cc75e8387e3713728e0bfb33
Santosh <santosh.kumar.jallepalli@netcracker.com>
2023-04-04 15:27:20 +0530

    commiting the basic project structure for the springboot customization

---
+++ src/main/java/com/example/springbootcustomizations/SpringbootCustomizationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootcustomizations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCustomizationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCustomizationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootcustomizations/SpringbootCustomizationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootcustomizations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCustomizationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
