9948541418dca25ee94c3313c3b02eff81c54bec
Santosh <santosh.kumar.jallepalli@netcracker.com>
2023-04-11 18:50:46 +0530

    Merging the Log file creation code

--- src/main/java/com/example/springbootcustomizations/SpringbootCustomizationsApplication.java
+++ src/main/java/com/example/springbootcustomizations/SpringbootCustomizationsApplication.java
@@ -1,9 +1,13 @@
 package com.example.springbootcustomizations;
 
+import com.example.springbootcustomizations.api.LoggerApi;
 import com.example.springbootcustomizations.service.HelloWorldService;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootCustomizationsApplication {
@@ -13,5 +17,17 @@ public class SpringbootCustomizationsApplication {
                 SpringApplication.run(SpringbootCustomizationsApplication.class, args);
         HelloWorldService service = context.getBean(HelloWorldService.class);
         System.out.println(service.getMessage());
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(LoggerApi logapi) {
+        System.out.println("Command Line Runner");
+        return args -> { logapi.generateLog("myCommandLog"); };
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner(LoggerApi logapi) {
+        System.out.println("Application Runner");
+        return args -> { logapi.generateLog("myApplicationLog"); };
     }
 }
